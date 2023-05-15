82a4246037a1af4155806cace3b27b6520e0e6a8
seandavidreed <seanreed7992@gmail.com>
2023-05-15 14:21:23 -0700

    Replace main.py with main2.py and rename

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-# To silence the NumbaDeprecationWarning, I added nopython=True
-# to the decorator on line 57 of
-# /home/<USERNAME>/.local/lib/python3.10/site-packages/whisper/timing.py
-
 import whisper
 import os
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, ttk
 
 # Example formatting to tell the model
 # how to punctuate the output and to
@@ -16,62 +12,107 @@ INITIAL_PROMPT = (
     "Do you Agree?"
 )
 
-class AudioFile():
+class Model:
     def __init__(self):
-        self.filename = None
-        self.generated_text = None
+        self.audio_filename = None
+        self.text_filename = None
+        self.audio_file = None
+        self.text_file = None
     
-    def open(self):
-        self.filename = filedialog.askopenfilename()
-    
-    def to_text(self):
-        '''
-        Simple wrapper method for the entre of the program:
-        the speech-to-text conversion.
-        '''
-        if self.filename == None:
-            return
+    def open_audio_file(self):
+        self.audio_filename = filedialog.askopenfilename()
 
+    def speech_to_text(self): 
         model = whisper.load_model("medium")
-        result = model.transcribe(audio=self.filename, initial_prompt=INITIAL_PROMPT, fp16=False, language='en')
-        self.generated_text = result["text"]
+        result = model.transcribe(audio=self.audio_filename, initial_prompt=INITIAL_PROMPT, fp16=False, language='en')
+        self.text_file = result["text"]
+        self.replace_extension(new_extension=".docx")
 
-
-# Replaces the file extension of the audio file
-# with that of the output text file.
-def replace_extension(filename, new_extension):
-    '''
-    os.path.splitext splits the filename into a tuple ('filename', '.extension')
-    We take the first part (the filename without the extension), 
-    and add the new extension to it.
-    '''
-    return os.path.splitext(filename)[0] + new_extension
-
-
-def main():
-    root = tk.Tk()
-    root.geometry("400x400")
-    root.title("Text To Speech")
-    label = tk.Label(root, text="Open File")
-    label.pack()
-
-    audio_file = AudioFile()
-    button = tk.Button(root, text="Select", command=audio_file.open)
-    button.pack()
-
-    button2 = tk.Button(root, text="Generate", command=audio_file.to_text)
-    button2.pack()
+        with open(self.text_filename, "w") as file:
+            for line in self.text_file:
+                file.write(line)
     
-    root.mainloop()
-    
-    # Prepare name for new text file.
-    text_filename = replace_extension(audio_file.filename, '.docx')
+    # Replaces the file extension of the audio file
+    # with that of the output text file.
+    def replace_extension(self, new_extension):
+        '''
+        os.path.splitext splits the filename into a tuple ('filename', '.extension')
+        We take the first part (the filename without the extension), 
+        and add the new extension to it.
+        '''
+        self.text_filename =  os.path.splitext(self.audio_filename)[0] + new_extension
 
-    # Write generated text to file.
-    with open(text_filename, "w") as file:
-        for line in audio_file.generated_text:
-            file.write(line)
 
+class View(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.title = ttk.Label(self, text="Speech to Text")
+
+        self.button = ttk.Button(self, text="Select File")
+        self.button.grid(row=0, column=0, padx=10)
+
+        self.label = ttk.Label(self, text="")
+        self.label.grid(row=1, column=0, padx=10)
+
+        self.button2 = ttk.Button(self, text="Generate Text")
+        self.button2.grid(row=3, column=0, padx=10)
+
+        self.label2 = ttk.Label(self, text="")
+        self.label2.grid(row=4, column=0, padx=10)
+
+        self.controller = None
+
+    def display_selected(self, filename):
+        self.label.config(text=filename)
+
+    def display_generating(self):
+        self.label2.config(text="Generating text...")
+
+    def display_nofile(self):
+        self.label2.config(text="No File Selected")
+
+    def set_controller(self, controller):
+        self.controller = controller
+
+
+class Controller:
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+
+        self.view.button.config(command=self.open)
+
+        self.view.button2.config(command=self.generate)
+
+    def open(self):
+        self.view.display_selected(self.model.audio_filename)
+        self.model.open_audio_file()
+        
+    def generate(self):
+        if self.model.audio_filename is None:
+            self.view.display_nofile()
+            return
+        self.view.display_generating()
+        self.model.speech_to_text()
+        
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Text to Speech App")
+
+        model = Model()
+
+        view = View(self)
+        view.grid(row=0, column=0, padx=10, pady=10)
+
+        controller = Controller(model, view)
+
+        view.set_controller(controller)
+
+        
 if __name__ == "__main__":
-    main()
-    
+    app = App()
+    app.mainloop()
